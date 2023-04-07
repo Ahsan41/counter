6c29c09de9800e13525911d99161974a2aa6485c
Ahsan41 <119751735+Ahsan41@users.noreply.github.com>
2023-04-08 00:56:04 +0500

    working

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let count = 0;
 document.getElementById(`increasebtn`).onclick= function(){
- count=+1;
- document.getElementById(`countlabel`).innerHTML= count;
+ count+=1;
+ document.getElementById(`countlabel`).innerText= count;
 }
 document.getElementById(`RESET`).onclick= function(){
     count=0;
-    document.getElementById(`countlabel`).innerHTML= count;
+    document.getElementById(`countlabel`).innerText= count;
    }
    document.getElementById(`DECREASEbtn`).onclick= function(){
-    count=-1;
-    document.getElementById(`countlabel`).innerHTML= count;
+    count-=1;
+    document.getElementById(`countlabel`).innerText= count;
    }
